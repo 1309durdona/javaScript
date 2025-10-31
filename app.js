1ca10af4113b81e2371565a1fcd402fcf1fafffb
Jumaniyoz5112 <5112jumaniyoz@gmail.com>
2025-10-31 09:52:15 +0500

    Add student data structure and log output to console

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log(16e-4);
+
+let mas = ["Muminniyoz", "Jumaniyoz", "Niyozbek", "Shonazarovlar"];
+
+console.log(mas);
+
+let talaba = {
+    ism: "Jumaniyoz",
+    familiya: "Shonazarov",
+    yoshi: 25,
+    malumoti: "Oliy",
+    kursi: 4,
+    fakulteti: "Informatika va axborot texnologiyalari",
+    oila: {
+        ota: "Murodov Shomurod",
+        ona: "Eliyeva Gulzoda",
+        opa: "Shonazaova Izzatoy",
+        aka: "Shonazarov Muminniyoz",
+        singil: "Shonazarova Hulkaroy",
+        uka: "Shonazarov Niyozbek",
+        rafiqa: "Tayloqova Durdona",
+        bolalar: {
+           ism: "Shomurova Soliha",
+            malumot: {
+                yoshi: 5,
+                maktabi: "Maktab 1"
+            }
+        }
+    }
+}
+
+console.log(talaba);
