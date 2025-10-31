8b71bfcf307b8f1d5b198bd27b78ba9802d57968
Jumaniyoz5112 <5112jumaniyoz@gmail.com>
2025-10-31 10:03:48 +0500

    Refactor app.js to remove unused variables and functions, and add a new function for addition

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
 
-let a;
-let b = undefined;
-let c = null;
+let c = 10;
+let fun = function(a, b) {
+    var c = 5;
+    return a + b;
+}
+console.log(c);
 
-console.log(a); // undefined
-console.log(b); // undefined
-console.log(c); // null
-
-a = 10;
-console.log(a);
-a = undefined;
-console.log(a);
-
-let ism = "";
-console.log(ism); // empty string
-console.log(typeof ism);
-
-let malumot = {ism: "Ali", yoshi: 25};
-console.log(malumot);
-console.log(typeof malumot.ism); // string
-console.log(typeof malumot.yoshi); // number
-
-malumot = null;
-console.log(malumot); // null
-console.log(typeof malumot); // object
-
-
-console.log(undefined == null);
-console.log(undefined === null);
-
-let mass = [1, 2, 3];
-console.log(mass[5]); // undefined
-console.log(typeof mass);
-
-
-
-
+console.log(fun(3, 4));
+console.log(fun);
