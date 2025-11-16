ed8abe4cec341374b9fd964274788dd469bae1ad
Jumaniyoz5112 <5112jumaniyoz@gmail.com>
2025-11-16 07:32:07 +0500

    Refactor app.js to remove unused person object and related code; streamline salom function

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
-var person = new Object();
-person.firstName = "Ali";
-person.lastName = "Valiyev";
-person.age = 45;
-person.eyeColor = "qora";
 
-console.log(typeof person);
-console.log(person);
 
-let x = person;
-x.age = 50;
-console.log(x);
-console.log(person);
-
-let txt = "";
-for(s in person){
-    txt += person[s] + " " ;
+function salom(ism){
+    console.log("salom, " + ism + " men " + this.role + "man");
+    
 }
-console.log(txt);  
 
-
- 
-let mas = txt.split();   
-console.log(mas);
-
-delete person.age;
-delete person.lastName;
-console.log(person);
-
-
-
-
-
-
-
-// var person = {firstName:"Ali", lastName:"Valiyev", age:45, eyeColor:"qora"};
-// var x = person;
-// x.age = 10;
-// console.log(person.age);
+const user = {role: "O'qituvchi"};
+salom.call(user, "Jumaniyoz");
