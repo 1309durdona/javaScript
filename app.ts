35ad9068cd3cf4e674504d476c92483e512d30ff
Jumaniyoz5112 <5112jumaniyoz@gmail.com>
2025-11-17 12:40:22 +0500

    Add array declarations and transform car names to uppercase in app.ts

---
+++ app.ts
@@ -0,0 +1,8 @@
+
+
+const cars : string[]= ["BMW", "Volvo", "Mini"];
+const num: number[] = [1,2,3,4,5];
+const bools: boolean[] = [true, false, true];
+
+const upparCaseCars = cars.map(car =>car.toUpperCase());
+console.log(upparCaseCars);
